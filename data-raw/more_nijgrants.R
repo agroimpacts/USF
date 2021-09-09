4d76f0ad0ec1ca7f695138932a80024e1cbc4574
pdelpinomarimon <pdelpinomarimon@clarku.edu>
2021-09-09 17:00:23 -0400

    edited raw

--- data-raw/more_nijgrants.R
+++ data-raw/more_nijgrants.R
@@ -1,4 +1,8 @@
 ## code to prepare `more_nijgrants` dataset goes here
+library(dplyr)
 setwd("C:/Users/pilii/Documents/Clark/RA-ing/SummerInstitute/")
 more_nijgrants <- read.csv("NIJ_Baltimore/more_nijgrants.csv")
+
+names(more_nijgrants)[3] <-  "Solicitor"
+names(more_nijgrants)[1] <- "Fiscal_Y"
 usethis::use_data(more_nijgrants, overwrite = TRUE)
