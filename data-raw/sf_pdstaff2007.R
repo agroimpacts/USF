83c2ca050329902caaaeabba4bc11e00c00e2b01
pdelpinomarimon <pdelpinomarimon@clarku.edu>
2021-10-25 19:17:10 -0400

    staff

--- data-raw/sf_pdstaff2007.R
+++ data-raw/sf_pdstaff2007.R
@@ -1,3 +1,4 @@
 ## code to prepare `sf_pdstaff2007` dataset goes here
-
+sf_pdstaff2007 <- read.csv("~/Clark/RA-ing/SummerInstitute/budget/sf_policestaffing2007.csv")
+names(sf_pdstaff2007)[1] <- "PD.District"
 usethis::use_data(sf_pdstaff2007, overwrite = TRUE)
