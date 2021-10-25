898e944491f43aa7bca1df18fe52738981efe5de
pdelpinomarimon <pdelpinomarimon@clarku.edu>
2021-10-25 14:24:17 -0400

    rmsdata

--- data-raw/rmsdata.R
+++ data-raw/rmsdata.R
@@ -1,3 +1,6 @@
 ## code to prepare `rmsdata` dataset goes here
+library(here)
+library(geospaar)
+rmsdata <- st_read("~/Clark/RA-ing/SummerInstitute/USF/localdrive/rms_data.shp")
 
 usethis::use_data(rmsdata, overwrite = TRUE)
