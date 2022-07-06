470495046957377d53ce271a1d72f08124334555
pdelpinomarimon <pdelpinomarimon@clarku.edu>
2022-07-06 17:20:38 -0400

    data needs line

--- script/shiny_housingjustice.R
+++ script/shiny_housingjustice.R
@@ -7,9 +7,12 @@
 # have all maps created in advance. These will go in the server section.
 # create leaflet maps of things we need to answer the question on housing justice
 
-# data needs
-# land use - For 2007: assessed value of land + lu_codes. 'bldg_desc_sp'. Edit
-# to include parcel information.
+# data needs, 2007
+# land use - For 2007: assessed value of land + lu_codes. 'bldg_desc_sp'. See: nyc_brooklyn_historicLU.R
+# neighborhood change - capital investments? census, income information
+# police presence - stop and frisk, or RTM
+
+
 
 # Skeleton
 library(shiny)
@@ -17,7 +20,7 @@ library(shiny)
 library(shinydashboard)
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Housing Justice - test"),
+  dashboardHeader(title = "Housing Justice, 2007 - test"),
   dashboardSidebar(), # on the left
   dashboardBody(
     box(plotOutput("map_1"), width = 8), # add box to dashboard
