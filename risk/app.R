959a57d123c55736e6292c300c1e6603e67317cd
pdelpinomarimon <pdelpinomarimon@clarku.edu>
2022-07-09 14:37:58 -0400

    sketch risk app

--- risk/app.R
+++ risk/app.R
@@ -8,45 +8,83 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(sp)
+library(sf)
+
 setwd(here("risk/"))
+bk07crime <- readRDS("bk07crime.RDS")
+rtm <- readRDS("rtm.RDS")
+parcels <- readRDS("parcels.RDS")
 
 
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
+    titlePanel("Deconstructing risk for property crime in BK, 2007 - test"),
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+        h5("Risk locations are defined by XYZ. Not all places are equally
+               risky through out the day."),
+        verticalLayout("Risky locations for that hour of day",
+                       tableOutput(outputId = "hist"))
         ),
-
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            sliderInput("range", "Select an hour of the day to begin",
+                        min = min(bk07crime$HourFormat),
+                        max = max(bk07crime$HourFormat),
+                        value = min(bk07crime$HourFormat),
+                        step = 1,
+                        animate = animationOptions(interval = 1000, loop = FALSE)),
+            leafletOutput("map")
         )
-    )
-)
+    ))
 
-# Define server logic required to draw a histogram
+
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    hourofday <- reactive({
+        h <- bk07crime %>% filter(HourFormat == input$range)
+        return(h)
     })
+
+    output$map <- renderLeaflet({
+      #  hourofday() %>%
+        leaflet() %>%
+            addProviderTiles(providers$CartoDB.Positron) %>%
+            setView(-73.95, 40.66, zoom = 11) #%>%
+            # addPolygons(stroke = TRUE, fill = TRUE,
+            #     color= NA, opacity = 5,
+            #     weight = 7,
+            #     fillOpacity = 6, fillColor = parcels$DECODES2007) %>%
+            # addLayersControl(
+            #     overlayGroups =  "Land Use",
+            #     options = layersControlOptions(collapsed = FALSE))
+    })
+
+    observe({
+        leafletProxy("map", data = hourofday()) %>%
+            clearShapes() %>%
+            addCircles(radius = 2, weight = 5, color = "#5310c4",
+                       fillOpacity = 0.05, opacity = 0.17,
+                       stroke = TRUE)
+
+    })
+
+    output$hist <- renderTable({
+
+        rtm %>% filter(HourFormat == input$range) %>%
+            filter(!PREM_TYP_DESC == "STREET") %>%
+            filter(!PREM_TYP_DESC %in% NA) %>%
+            filter(!PREM_TYP_DESC == "") %>%
+            group_by(.$PREM_TYP_DESC) %>%
+            count() %>% # frequency per group
+            rename(., RTMfactors = ".$PREM_TYP_DESC", Count = "n") %>%
+            as_tibble() %>% arrange(-Count)
+
+
+    })
+
 }
 
-# Run the application
 shinyApp(ui = ui, server = server)
