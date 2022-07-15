868746ae438c1336ecb302fae5e996caa62fb62c
pdelpinomarimon <pdelpinomarimon@clarku.edu>
2022-07-15 11:50:13 -0400

    nyctract script

--- data-raw/nytract.R
+++ data-raw/nytract.R
@@ -1,3 +1,12 @@
 ## code to prepare `nytract` dataset goes here
+library(sp)
+library(sf)
+library(here)
+
+# source of nyctract:
+# code to extract
+# downloaded in a particular file (home)
+
+nytract <- st_read("~/Clark/RA-ing/SummerInstitute/nightlight/data/nyct2020.shp")
 
 usethis::use_data(nytract, overwrite = TRUE)
