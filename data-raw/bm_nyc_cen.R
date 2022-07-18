3db308e5fc74bbf501aeba204698b21a8fcac4e8
pdelpinomarimon <pdelpinomarimon@clarku.edu>
2022-07-18 12:05:25 -0400

    update raster RDA

--- data-raw/bm_nyc_cen.R
+++ data-raw/bm_nyc_cen.R
@@ -1,5 +1,6 @@
 ## code to prepare `bm_nyc_cen` dataset goes here
 #NYC data - Nighttime Lights (2012-2022 -end 2021)
 library(raster)
-bm_nyc_cen <- stack("C://CLARK//GEOSPAAR//floodlightblm//data//bm_nyc_cen.grd")
+library(here)
+bm_nyc_cen <- raster("~/Clark/RA-ing/SummerInstitute/nightlight/data/bm_nyc_cen.grd")
 usethis::use_data(bm_nyc_cen, overwrite = TRUE)
