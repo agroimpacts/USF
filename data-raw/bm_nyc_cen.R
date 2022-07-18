0651f4564208d9abdb4ec0280deec13bb43fd8ec
pdelpinomarimon <pdelpinomarimon@clarku.edu>
2022-07-18 13:43:03 -0400

    delete bm_nyc_cen rda

--- data-raw/bm_nyc_cen.R
+++
@@ -1,6 +0,0 @@
-## code to prepare `bm_nyc_cen` dataset goes here
-#NYC data - Nighttime Lights (2012-2022 -end 2021)
-library(raster)
-library(here)
-bm_nyc_cen <- raster("~/Clark/RA-ing/SummerInstitute/nightlight/data/bm_nyc_cen.grd")
-usethis::use_data(bm_nyc_cen, overwrite = TRUE)
