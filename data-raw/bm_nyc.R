64c64ed03326c7e1d9019dee7907b1fa929733c2
pdelpinomarimon <pdelpinomarimon@clarku.edu>
2022-07-18 12:05:25 -0400

    update raster RDA

--- data-raw/bm_nyc.R
+++ data-raw/bm_nyc.R
@@ -1,5 +1,6 @@
 ## code to prepare `bm_nyc` dataset goes here
 #NYC Grid from Black Marble
 library(raster)
-bm_nyc <- stack("C://CLARK//GEOSPAAR//floodlightblm//data//bm_nyc.grd")
+library(here)
+bm_nyc <- raster("~/Clark/RA-ing/SummerInstitute/nightlight/data/bm_nyc.grd")
 usethis::use_data(bm_nyc, overwrite = TRUE)
