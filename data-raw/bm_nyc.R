1e514398bacf0fa7c86691ab4be229a46842e822
pdelpinomarimon <pdelpinomarimon@clarku.edu>
2022-07-21 17:16:52 -0400

    clean up repo

--- data-raw/bm_nyc.R
+++
@@ -1,6 +0,0 @@
-## code to prepare `bm_nyc` dataset goes here
-#NYC Grid from Black Marble
-library(raster)
-library(here)
-bm_nyc <- raster("~/Clark/RA-ing/SummerInstitute/nightlight/data/bm_nyc.grd")
-usethis::use_data(bm_nyc, overwrite = TRUE)
