7adb3b5d40215f3635f3ab69dcfcff601b49e92d
pdelpinomarimon <pdelpinomarimon@clarku.edu>
2023-03-19 16:35:55 -0500

    edit

--- script/website/test_casestudy1a.R
+++ script/website/test_casestudy1a.R
@@ -11,14 +11,14 @@ library(sf)
 library(shinydashboard)
 library(readr)
 
-setwd("~/Clark/RA-ing/SummerInstitute/USF/housing-justice")
+setwd("~/housing-justice")
 parcels2007 <- readRDS("parcels2.RDS")
 #parcels <- readRDS("parcels.RDS")
 #parcels <- readRDS("q.RDS") #Williamsburg test
 bk_priority2007 <- readRDS("bk_priority.RDS")
 brooklyn_neigh <- readRDS("brooklyn_neigh.RDS")
 
-setwd("~/Clark/RA-ing/SummerInstitute/USF/housing-justice-2020")
+setwd("~/housing-justice-2020")
 parcels2020 <- readRDS("parcels2.RDS")
 #bk_priority <- readRDS("bk_priority.RDS") # replace with 2019 data
 brooklyn_neigh <- readRDS("brooklyn_neigh.RDS")
@@ -39,13 +39,18 @@ mapq_interactive <- leaflet() %>% # calls leaflet
     addProviderTiles(providers$CartoDB.Positron) %>% #base map
   addPolygons(data = bk_priority2007, # 2007 location (under layer)
               stroke = FALSE, fill = TRUE,
-              opacity = 2,
-              weight = 2,
+              opacity = 1,
+              weight = 5,
               color = "##a39091", #light grey red
-              fillOpacity = 2, fillColor = "#c97c77" ) %>%
-addPolygons(data = bk_priority2020, # 2020 location (over layer)
+              fillOpacity = 1, fillColor = "#c97c77", group = "Risk 2007") %>%
+  addPolygons(data = bk_priority2020, # 2020 location (over layer)
             stroke = FALSE, fill = TRUE,
             color= "#f52516", # hot red
-            opacity = 0.7,
-            weight = 2, # set the stroke width in pixels,
-            fillOpacity = 0.7, fillColor = "#f52516")
+            opacity = 0.8,
+            weight = 5, # set the stroke width in pixels,
+            fillOpacity = 0.8, fillColor = "#f52516", group = "Risk 2020") %>%
+  addLayersControl(position = "topright",
+                   overlayGroups = c("Risk 2007", "Risk 2020"),
+                   options = layersControlOptions(collapsed = FALSE))
+
+mapq_interactive
